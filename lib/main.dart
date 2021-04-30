6ede78a025fe979a2098ef09a8d6aff7769038c1
suna0 <suna0.o4o4gym@ezweb.ne.jp>
2021-04-30 12:01:29 +0900

    4/30 first

--- lib/main.dart
+++ lib/main.dart
@@ -9,26 +9,19 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('todo-app'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-          ],
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('todo-app'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'You have pushed the button this many times:',
+              ),
+            ],
+          ),
         ),
       ),
     );
