5b872aa28155e78a3e3969110fb1bec3f16f992d
suna0 <suna0.o4o4gym@ezweb.ne.jp>
2021-05-03 16:47:11 +0900

    firebaseとの同期

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:todo_app/add/add_page.dart';
 import 'package:todo_app/main_model.dart';
 
 void main() async {
@@ -14,26 +15,74 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      home: ChangeNotifierProvider<MainModel>(
-        create: (_) => MainModel()..getTodoList(),
-        child: Scaffold(
+      home: MainPage(),
+    );
+  }
+}
+
+class MainPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider<MainModel>(
+      create: (_) => MainModel()..getTodoListRealtime(),
+      child: Scaffold(
           appBar: AppBar(
             title: Text('todo-app'),
+            actions: [
+              // ignore: deprecated_member_use
+              Consumer<MainModel>(builder: (context, model, child) {
+                final isActive = model.checkShouldActiveCompleteButton();
+                // ignore: deprecated_member_use
+                return FlatButton(
+                  onPressed: isActive
+                      ? () async {
+                          await model.deleteCheckedItems();
+                        }
+                      : null,
+                  child: Text(
+                    '完了',
+                    style: TextStyle(
+                      color: isActive
+                          ? Colors.white
+                          : Colors.white.withOpacity(0.5),
+                    ),
+                  ),
+                );
+              }),
+            ],
           ),
           body: Consumer<MainModel>(builder: (context, model, child) {
             final todoList = model.todoList;
             return ListView(
               children: todoList
                   .map(
-                    (todo) => ListTile(
+                    (todo) => CheckboxListTile(
                       title: Text(todo.title),
+                      value: todo.isDone,
+                      onChanged: (bool value) {
+                        todo.isDone = !todo.isDone;
+                        model.reload();
+                      },
                     ),
                   )
                   .toList(),
             );
           }),
-        ),
-      ),
+          floatingActionButton:
+              Consumer<MainModel>(builder: (context, model, child) {
+            return FloatingActionButton(
+              onPressed: () async {
+                await Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => AddPage(model),
+                    fullscreenDialog: true,
+                  ),
+                );
+              },
+              child: Icon(Icons.add),
+            );
+          })),
     );
   }
 }
--- lib/main_model.dart
+++ lib/main_model.dart
@@ -4,6 +4,7 @@ import 'package:todo_app/todo.dart';
 
 class MainModel extends ChangeNotifier {
   List<Todo> todoList = [];
+  String newTodoText = '';
 
   Future getTodoList() async {
     final snapshot =
@@ -12,5 +13,48 @@ class MainModel extends ChangeNotifier {
     final todoList = docs.map((doc) => Todo(doc)).toList();
     this.todoList = todoList;
     notifyListeners();
+  }
+
+  void getTodoListRealtime() {
+    final snapshots =
+        FirebaseFirestore.instance.collection('todoList').snapshots();
+    snapshots.listen((snapshot) {
+      final docs = snapshot.docs;
+      final todoList = docs.map((doc) => Todo(doc)).toList();
+      todoList.sort((a, b) => b.createdAt.compareTo(a.createdAt));
+      this.todoList = todoList;
+      notifyListeners();
+    });
+  }
+
+  Future add() async {
+    final collection = FirebaseFirestore.instance.collection('todoList');
+    await collection.add({
+      'title': newTodoText,
+      'createdAt': Timestamp.now(),
+    });
+  }
+
+  void reload() {
+    notifyListeners();
+  }
+
+  Future deleteCheckedItems() async {
+    final checkedItems = todoList.where((todo) => todo.isDone).toList();
+    final references =
+        checkedItems.map((todo) => todo.documentReference).toList();
+
+    final batch = FirebaseFirestore.instance.batch();
+
+    references.forEach((reference) {
+      batch.delete(reference);
+    });
+
+    return batch.commit();
+  }
+
+  bool checkShouldActiveCompleteButton() {
+    final checkedItems = todoList.where((todo) => todo.isDone).toList();
+    return checkedItems.length > 0;
   }
 }
